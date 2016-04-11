23bc7f826aadc748ed4853ab52bc789c927b918d
Dee Reddy <d.reddy@yandex.com>
2016-04-11 10:01:37 -0400

    feat: Add scripts.

---
+++ src/js/scripts.js
@@ -0,0 +1,42 @@
+/*========================================
+	FlexFramewokr - UI Scripts
+  ========================================*/
+// TODO: Refactor jQuery scripts to vanilla JS
+
+
+
+$(document).ready(function() {
+	
+	/****************************
+	 * Affix
+	 * 
+	 * Fixes DOM element when user scrolls (e.g. fixed menu bar)
+	 ****************************/
+	// for `data-watcher` attributes set to `affix`
+	// also need to set `data-offset` (to specify top/right position)
+	// e.g. `<nav class='nav' data-watcher='affix' data-offset-top='200'>`
+
+	// TODO: need to do for every element set to `affix` (i.e. loop)
+	// .addClass('affix');
+	
+	
+	/****************************
+	 * Collapse/ Collapse group
+	 *
+	 * Hide and show elements via 'blinds'
+	 ****************************/
+	
+	
+	/****************************
+	 * Dropdown
+	 *
+	 * creates drop-down menus after after clicking
+	 ****************************/
+	
+	/***************************
+	 * Carosel
+	 *
+	 * cycles through elements -- e.g. slideshow
+	 ***************************/
+	
+});
